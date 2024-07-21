8c5c7fccfbc97e3a78272e4e9dd97e5fb152f946
Mari Montoya <kulafumon@gmail.com>
2024-07-21 18:13:58 -0400

    Created basic version of game.

--- pong.py
+++ pong.py
@@ -1,13 +1,15 @@
 import os
 import pygame
+from random import randint, uniform
 
 # Screen Constants
 FPS = 60
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 576
+CENTER_LINE_WIDTH = 10
 
 # Player Constants
-PLAYER_WIDTH = 30
+PLAYER_WIDTH = 20
 PLAYER_HEIGHT = 100
 PLAYER_VELOCITY = 15
 
@@ -15,14 +17,25 @@ PLAYER_VELOCITY = 15
 BALL_SIZE = 15
 
 # Colour Constants
+GRAY = (128, 128, 128)
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Fonts
+pygame.font.init()
+SCORE_FONT = pygame.font.Font('./assets/RubikMonoOne-Regular.ttf', 48)
+
 pygame.display.set_caption('Pong')
+WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+CENTER_LINE = pygame.Rect(SCREEN_WIDTH / 2 - CENTER_LINE_WIDTH / 2, 0, CENTER_LINE_WIDTH, SCREEN_HEIGHT)
 
 # Helpers
 
-
+def resetBall(aBall, aBallVelocity):
+  aBall.x = SCREEN_WIDTH / 2 - BALL_SIZE / 2
+  aBall.y = SCREEN_HEIGHT / 2 - BALL_SIZE / 2
+  aBallVelocity['x'] = 5 if randint(0, 1) == 1 else -5
+  aBallVelocity['y'] = 5 if randint(0, 1) == 1 else -5
 
 # Actual Game
 
@@ -31,15 +44,15 @@ def main():
   clock = pygame.time.Clock()
   
   P1 = pygame.Rect(SCREEN_WIDTH / 16, SCREEN_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
-  P2 = pygame.Rect(SCREEN_WIDTH - SCREEN_WIDTH / 10, SCREEN_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
+  P2 = pygame.Rect(SCREEN_WIDTH - SCREEN_WIDTH / 16 - PLAYER_WIDTH, SCREEN_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
   BALL = pygame.Rect(SCREEN_WIDTH / 2 - BALL_SIZE / 2, SCREEN_HEIGHT / 2 - BALL_SIZE / 2, BALL_SIZE, BALL_SIZE)
   
   P1Score = 0
   P2Score = 0
   
   BALL_VELOCITY = {
-    'x':5,
-    'y':5
+    'x':5 if randint(0, 1) == 1 else -5,
+    'y':5 if randint(0, 1) == 1 else -5
   }
 
   while run:
@@ -73,20 +86,31 @@ def main():
       
     BALL.x += BALL_VELOCITY['x']
     BALL.y += BALL_VELOCITY['y']
-    if BALL.x >= SCREEN_WIDTH - BALL_SIZE or BALL.x <= 0:
-      BALL_VELOCITY['x'] *= -1
     if BALL.y >= SCREEN_HEIGHT - BALL_SIZE or BALL.y <= 0:
-      BALL_VELOCITY['y'] *= -1
+      BALL_VELOCITY['y'] *= -1.1
+    if BALL.x >= SCREEN_WIDTH - BALL_SIZE:
+      P2Score += 1
+      resetBall(BALL, BALL_VELOCITY)
+    elif BALL.x <= 0:
+      P1Score += 1
+      resetBall(BALL, BALL_VELOCITY)
+    elif (P1.x <= BALL.x <= P1.x + PLAYER_WIDTH and P1.y <= BALL.y <= P1.y + PLAYER_HEIGHT) or (P2.x <= BALL.x + BALL_SIZE <= P2.x + PLAYER_WIDTH and P2.y <= BALL.y <= P2.y + PLAYER_HEIGHT):
+      BALL_VELOCITY['x'] *= -1.05
     
     # Game Display
     WINDOW.fill((0, 0, 0))
+    pygame.draw.rect(WINDOW, GRAY, CENTER_LINE)
     
     pygame.draw.rect(WINDOW, WHITE, P1)
     pygame.draw.rect(WINDOW, WHITE, P2)
     pygame.draw.rect(WINDOW, WHITE, BALL)
     
+    P1ScoreText = SCORE_FONT.render(f'{P1Score:02d}', False, GRAY)
+    P2ScoreText = SCORE_FONT.render(f'{P2Score:02d}', False, GRAY)
+    WINDOW.blit(P1ScoreText, (SCREEN_WIDTH / 2 + CENTER_LINE_WIDTH / 2 + SCORE_FONT.get_linesize() / 2, SCORE_FONT.get_linesize() / 2))
+    WINDOW.blit(P2ScoreText, (SCREEN_WIDTH / 2 - CENTER_LINE_WIDTH / 2 - SCORE_FONT.get_linesize() / 2 - P2ScoreText.get_width(), SCORE_FONT.get_linesize() / 2))
+    
     pygame.display.update()
 
 if __name__ == "__main__":
     main()
-pygame.quit()
